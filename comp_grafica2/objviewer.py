3c57afb3d12c101b4c4034c2ecf88fbae4d615ce
richardgss <142852268+richardgss@users.noreply.github.com>
2024-04-24 08:39:37 -0300

    Add files via upload

---
+++ comp_grafica2/objviewer.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# Basic OBJ file viewer. needs objloader from:
+#  http://www.pygame.org/wiki/OBJFileLoader
+# LMB + move: rotate
+# RMB + move: pan
+# Scroll wheel: zoom in/out
+import sys, pygame
+from pygame.locals import *
+from pygame.constants import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+# IMPORT OBJECT LOADER
+from objloader import *
+
+pygame.init()
+viewport = (800,600)
+hx = viewport[0]/2
+hy = viewport[1]/2
+srf = pygame.display.set_mode(viewport, OPENGL | DOUBLEBUF)
+
+glLightfv(GL_LIGHT0, GL_POSITION,  (-40, 200, 100, 0.0))
+glLightfv(GL_LIGHT0, GL_AMBIENT, (0.2, 0.2, 0.2, 1.0))
+glLightfv(GL_LIGHT0, GL_DIFFUSE, (0.5, 0.5, 0.5, 1.0))
+glEnable(GL_LIGHT0)
+glEnable(GL_LIGHTING)
+glEnable(GL_COLOR_MATERIAL)
+glEnable(GL_DEPTH_TEST)
+glShadeModel(GL_SMOOTH)           # most obj files expect to be smooth-shaded
+
+# LOAD OBJECT AFTER PYGAME INIT
+obj = OBJ(sys.argv[1], swapyz=True)
+obj.generate()
+
+clock = pygame.time.Clock()
+
+glMatrixMode(GL_PROJECTION)
+glLoadIdentity()
+width, height = viewport
+gluPerspective(90.0, width/float(height), 1, 100.0)
+glEnable(GL_DEPTH_TEST)
+glMatrixMode(GL_MODELVIEW)
+
+rx, ry, rz = (0,0,0)
+tx, ty = (0,0)
+zpos = 5
+rotate = rotate2 = move = False
+
+
+while 1:
+    clock.tick(30)
+    for e in pygame.event.get():
+        if e.type == QUIT:
+            sys.exit()
+        elif e.type == KEYDOWN and e.key == K_ESCAPE:
+            sys.exit()
+        elif e.type == MOUSEBUTTONDOWN:
+            print("DOWN - e.button", e.button)
+            if e.button == 4: zpos = max(1, zpos-1)
+            elif e.button == 5: zpos += 1
+            elif e.button == 3: rotate2 = True
+            elif e.button == 1: rotate = True
+            elif e.button == 2: move = True
+        elif e.type == MOUSEBUTTONUP:
+            print("UP - e.button", e.button)
+            if e.button == 1: rotate = False
+            elif e.button == 3: rotate2 = False
+            elif e.button == 2: move = False
+        elif e.type == MOUSEMOTION:
+            i, j = e.rel
+            if rotate:
+                rx += i
+                ry += j
+            if rotate2:
+                rz += i
+            if move:
+                tx += i
+                ty -= j
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glLoadIdentity()
+
+    # print("rx: ", rx, " - ry: ", ry, " - rz: ", " - tx: ", tx, " - ty: ", ty, " - zpos: ", zpos)
+
+    # RENDER OBJECT
+    glTranslate(tx/20., ty/20., - zpos)
+    glRotate(ry, 1, 0, 0)
+    glRotate(rx, 0, 1, 0)
+    glRotate(rz, 0, 0, 1)
+    obj.render()
+
+    pygame.display.flip()
